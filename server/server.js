c78ac318ae91e0c49edffa54b863554ec16327bc
Abby <6406244@gmail.com>
2022-05-19 19:41:36 +0300

    added shortid

---
+++ server/server.js
@@ -0,0 +1,91 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import sockjs from 'sockjs'
+import { renderToStaticNodeStream } from 'react-dom/server'
+import React from 'react'
+
+import cookieParser from 'cookie-parser'
+import config from './config'
+import Html from '../client/html'
+
+require('colors')
+
+let Root
+try {
+  // eslint-disable-next-line import/no-unresolved
+  Root = require('../dist/assets/js/ssr/root.bundle').default
+} catch {
+  console.log('SSR not found. Please run "yarn run build:ssr"'.red)
+}
+
+let connections = []
+
+const port = process.env.PORT || 8090
+const server = express()
+
+const middleware = [
+  cors(),
+  express.static(path.resolve(__dirname, '../dist/assets')),
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
+  express.json({ limit: '50mb', extended: true }),
+  cookieParser()
+]
+
+middleware.forEach((it) => server.use(it))
+
+const task = {  
+      taskId: '2WEKaVNO',  // используйте https://www.npmjs.com/package/shortid для создания нового айти при вставке -  shortid.generate(),  
+      title: 'Task', // имя таска  
+      _isDeleted: false, // флаг удален ли таск. Физичически мы таски не удаляем, только помечаем что удален  
+      _createdAt: 14135235б, // время в секундах от 1,1,1970 до момента создания таска, // read utc format ( +new Date() )  
+      _deletedAt: 14135235б, // время в секундах от 1,1,1970 до момента удаление таска или null, // read utc format ( +new Date() )  
+      status: 'done'  // ['done', 'new', 'in progress', 'blocked'] - может быть только эти значения и никакие больше  
+  }  
+
+
+server.use('/api/', (req, res) => {
+  res.status(404)
+  res.end()
+})
+
+const [htmlStart, htmlEnd] = Html({
+  body: 'separator',
+  title: 'Skillcrucial'
+}).split('separator')
+
+server.get('/', (req, res) => {
+  const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
+  res.write(htmlStart)
+  appStream.pipe(res, { end: false })
+  appStream.on('end', () => {
+    res.write(htmlEnd)
+    res.end()
+  })
+})
+
+server.get('/*', (req, res) => {
+  const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
+  res.write(htmlStart)
+  appStream.pipe(res, { end: false })
+  appStream.on('end', () => {
+    res.write(htmlEnd)
+    res.end()
+  })
+})
+
+const app = server.listen(port)
+
+if (config.isSocketsEnabled) {
+  const echo = sockjs.createServer()
+  echo.on('connection', (conn) => {
+    connections.push(conn)
+    conn.on('data', async () => {})
+
+    conn.on('close', () => {
+      connections = connections.filter((c) => c.readyState !== 3)
+    })
+  })
+  echo.installHandlers(app, { prefix: '/ws' })
+}
+console.log(`Serving at http://localhost:${port}`)
